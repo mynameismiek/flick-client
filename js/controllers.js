b1069cc00ece52e600a8ae19377906d03fdc7bd5
Michael Hope <mynameismiek@gmail.com>
2016-03-02 00:13:36 -0500

    initial ionic checkin

---
+++ js/controllers.js
@@ -0,0 +1,18 @@
+angular.module('app.controllers', [])
+  
+.controller('browseFlicksCtrl', function($scope) {
+
+})
+   
+.controller('matchesCtrl', function($scope) {
+
+})
+   
+.controller('profileCtrl', function($scope) {
+
+})
+   
+.controller('settingsCtrl', function($scope) {
+
+})
+    
---
+++ js/routes.js
@@ -0,0 +1,63 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('flick.browseFlicks', {
+    url: '/browse',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/browseFlicks.html',
+        controller: 'browseFlicksCtrl'
+      }
+    }
+  })
+
+  .state('flick.matches', {
+    url: '/matches',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/matches.html',
+        controller: 'matchesCtrl'
+      }
+    }
+  })
+
+  .state('flick.profile', {
+    url: '/profile',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/profile.html',
+        controller: 'profileCtrl'
+      }
+    }
+  })
+
+  .state('flick.settings', {
+    url: '/settings',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/settings.html',
+        controller: 'settingsCtrl'
+      }
+    }
+  })
+
+  .state('flick', {
+    url: '/side-menu21',
+    templateUrl: 'templates/flick.html',
+    abstract:true
+  })
+
+$urlRouterProvider.otherwise('/side-menu21/browse')
+
+  
+
+});
