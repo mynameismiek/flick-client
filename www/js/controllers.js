d911a7b68bb2b153f854c6813ca9f91b3f6e4a09
Anders Martin <eears100@gmail.com>
2016-03-13 15:32:31 -0400

    Add executescript to return auth token from inappbrowser

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,34 +3,34 @@ angular.module('app.controllers', [])
 .controller('signupCtrl', function($scope) {
 
 })
-   
-.controller('loginCtrl', function($scope, $cordovaInAppBrowser) {
-	   $scope.facebookLogin = function(){
-           $cordovaInAppBrowser.open('http://localhost:9000/auth/facebook', '_system')
-           .then(function(event){
-               console.log(event);
-           })
-           .catch(function(event){
-               
-           })
-       } 
+
+.controller('loginCtrl', function($scope) {
+	$scope.facebookLogin = function(){
+		var fbLogin = window.open('http://localhost:9000/auth/facebook', '_blank');
+		fbLogin.addEventListener('loadstop', function(){
+			fbLogin.executeScript({code:'JSON.parse(document.body.innerText)'},function(res){
+				$scope.token = res[0].token;
+				$scope.$apply();		
+				fbLogin.close();
+			});
+		}); 
+	};
 })
-  
+
 .controller('browseFlicksCtrl', function($scope) {
 
 })
-   
+
 .controller('matchesCtrl', function($scope) {
 
 })
-   
+
 .controller('profileCtrl', function($scope) {
 
 })
-   
+
 .controller('settingsCtrl', function($scope) {
 
 })
 
 
-    
